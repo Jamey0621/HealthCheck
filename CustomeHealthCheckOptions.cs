6e30f25c943b5e961a49728622da232a245a528d
jamey geoghagan <jameygeoghagan@gmail.com>
2021-11-29 01:00:39 -0600

    Ang-front ASP.net - Back

---
+++ CustomeHealthCheckOptions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using System.Net.Mime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HealthCheck
+{
+    public class CustomeHealthCheckOptions : HealthCheckOptions
+    {
+        //Constructer
+        public CustomeHealthCheckOptions() : base()
+        {
+            var jsonSerializerOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            ResponseWriter = async (c, r) =>
+            {
+                c.Response.ContentType =
+                 MediaTypeNames.Application.Json;
+                c.Response.StatusCode = StatusCodes.Status200OK;
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    checks = r.Entries.Select(e => new
+                    {
+                        name = e.Key,
+
+                        responseTime =
+                             e.Value.Duration.TotalMilliseconds,
+                        status = e.Value.Status.ToString(),
+                        description = e.Value.Description
+                    }),
+                    totalStatus = r.Status,
+                    totalResponseTime =
+                       r.TotalDuration.TotalMilliseconds,
+                }, jsonSerializerOptions);
+                await c.Response.WriteAsync(result);
+            };
+        }
+
+        
+    }
+}
